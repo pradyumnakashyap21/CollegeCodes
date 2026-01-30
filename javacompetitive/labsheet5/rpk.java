9b27bbe32504e40cf9ab3442601efe191e1065df
pradyumnakashyap21 <pradyumnakashyap21@gmail.com>
2026-01-30 05:07:48 +0000

    upto labsheet 5

---
+++ javacompetitive/labsheet5/rpk.java
@@ -0,0 +1,18 @@
+package labsheet5;
+//Ncr labsheet 5
+import java.util.Scanner;
+
+public class rpk {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int r = sc.nextInt();
+        r = Math.min(r, n - r);
+        long result = 1;
+        for (int i = 1; i <= r; i++) {
+            result = result * (n - r + i) / i;
+        }
+        System.out.println(result);
+        System.out.println("R Pradyumna Kashyap 20231CSE0120");
+    }
+}
---
+++ javacompetitive/labsheet5/rr.java
@@ -0,0 +1,24 @@
+package labsheet5;
+ import java.util.Scanner;
+public class rr {
+    static long factorial(int n) {
+        long fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+        return fact;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        long result = factorial(n) / (factorial(k) * factorial(n - k));
+
+        System.out.println(result);
+        System.out.println("R Pradyumna Kashyap 20231CSE0120");
+    }
+}
+
+
