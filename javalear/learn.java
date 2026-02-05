68b1386d4843c67205f1a7b9f341b63ea88103b5
pradyumnakashyap21 <pradyumnakashyap21@gmail.com>
2026-02-05 05:08:51 +0000

    Labsheet5

---
+++ javalear/learn.java
@@ -0,0 +1,12 @@
+package javalear;
+
+public class learn {
+    public static void main(String[]args){
+        System.out.println("Hellow WOrld");
+        int n =2;
+        int n1= 3;
+        int n2 = 4;
+        int sum = n+n1+n2;
+        System.out.println("The sum of this "+sum);
+    }
+}
