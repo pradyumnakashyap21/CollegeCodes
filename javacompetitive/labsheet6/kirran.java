2ff4351f64ad9af242df339dbe48763bbb978900
pradyumnakashyap21 <pradyumnakashyap21@gmail.com>
2026-02-13 05:11:20 +0000

    labsheet 6

---
+++ javacompetitive/labsheet6/kirran.java
@@ -0,0 +1,11 @@
+imporrt java.util.*;
+public class kirran{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+    }
+}
