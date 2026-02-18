6f206e4d5624cd11094ecc38fd86dfa735a5153f
pradyumnakashyap21 <pradyumnakashyap21@gmail.com>
2026-02-18 18:16:45 +0000

    done somewhat

--- javacompetitive/labsheet6/kirran.java
+++ javacompetitive/labsheet6/kirran.java
@@ -1,11 +1,16 @@
-imporrt java.util.*;
+import java.util.*;
 public class kirran{
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int arr[] = new int[n];
+        int target = sc.nextInt();
         for(i=0; i<n; i++){
             arr[i] = sc.nextInt();
         }
+        if(target == arr[i]){
+            return i;
+        }
+        return -1;
     }
 }
