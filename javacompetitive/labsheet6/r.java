8cd32963281075f82cc057ce395e7646e1361418
pradyumnakashyap21 <pradyumnakashyap21@gmail.com>
2026-02-20 05:05:43 +0000

    labsheet 7

--- javacompetitive/labsheet6/r.java
+++ javacompetitive/labsheet6/r.java
@@ -1,15 +1,22 @@
+package labsheet6;
 import java.util.*;
-public class kirran{
-
+public class r{
+     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int result = 0;  
-
+        int r = sc.nextInt();
         for (int i = 0; i < n; i++) {
             int num = sc.nextInt();
             result = result ^ num; 
         }
-        System.out.println(result);
+        if(result == r){
+            System.out.println("OK");
+        }
+        else{
+            System.out.println("Anamoly");
+        }
+        System.out.println("R Pradyumna 20231CSE0120");
     }
 }
